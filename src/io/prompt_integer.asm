f98dd3e9a0ce620ded375ab49b304bf8c34115e4
Mitchell Ford <mitchelltford@gmail.com>
2021-08-18 18:10:43 -0500

    Add more driver code to main function and add comment to the top of the file

--- src/io/prompt_integer.asm
+++ src/io/prompt_integer.asm
@@ -1,16 +1,30 @@
 # This file contains a function that prompts the user to enter an integer and returns the input
+# Executing this file prompts the user for an integer then prints it back to them
 
 .data
 
 	prompt: .asciiz "Enter a number: "
+	result: .asciiz "You entered: "
 
 .text
 
 # Call promptInteger using the prompt string
 main:
-	# promptInteger("Enter a number: ")
+	# input = promptInteger("Enter a number: ")
 	la $a0 prompt
 	jal promptInteger
+	move $t0 $v0
+	
+	# Print the result string
+	la $a0 result
+	li $v0 4
+	syscall
+	
+	# Print the user's input
+	# printf("You entered: %d", input)
+	move $a0 $t0
+	li $v0 1
+	syscall
 	
 	# Terminate the program
 	li $v0 10
